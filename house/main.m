2a696be9023acc0047808e6598157ff270580de2
Andrew Kreps <andrew.kreps@gmail.com>
2013-02-24 00:32:41 -0800

    Initial Commit

---
+++ house/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  house
+//
+//  Created by Andrew Kreps on 2/24/13.
+//  Copyright (c) 2013 Andrew Kreps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
